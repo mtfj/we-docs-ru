bfb100d1c5234021712c3d91095fbbe644981f7b
Mikhail Milekhin <mikhail.milekhin@gmail.com>
2018-11-19 17:23:53 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -125,34 +125,6 @@ def setup(app):
 htmlhelp_basename = 'vostok-docs'
 
 
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'hyperledger-fabricdocs.tex', u'hyperledger-fabricdocs Documentation',
-     u'hyperledger', 'manual'),
-]
-
 
 # -- Options for manual page output ---------------------------------------
 
@@ -162,19 +134,6 @@ man_pages = [
     (master_doc, 'hyperledger-fabricdocs', u'hyperledger-fabricdocs Documentation',
      [author], 1)
 ]
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'hyperledger-fabricdocs', u'hyperledger-fabricdocs Documentation',
-     author, 'hyperledger-fabricdocs', 'One line description of project.',
-     'Miscellaneous'),
-]
-
 
 
 # -- Options for Epub output ----------------------------------------------
