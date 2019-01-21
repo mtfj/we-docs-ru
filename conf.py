33624c38a7c268b26c14b364ee08d678ae063b9c
MMilekhin <mmilekhin@wavesplatform.com>
2019-01-21 16:48:05 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -136,7 +136,7 @@ def setup(app):
     #app.add_stylesheet('css/custom.css')
     app.add_stylesheet('theme_override.css')
 
-html_logo = 'img/logo-1.png'
+# html_logo = 'img/logo-1.png'
 
 # -- Options for HTMLHelp output ------------------------------------------
 
