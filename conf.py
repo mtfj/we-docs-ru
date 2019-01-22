a04a49ce6088ea16ffa87e1f1637465e3720f7df
MMilekhin <mmilekhin@wavesplatform.com>
2019-01-22 12:27:03 +0300

    redesign logo section

--- conf.py
+++ conf.py
@@ -202,4 +202,4 @@ latex_logo = '_static/img/logo.png'
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+# intersphinx_mapping = {'https://docs.python.org/': None
