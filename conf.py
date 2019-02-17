d5a610ad2a7b1ff8279a3196ee10ed4eb0505c0e
Mikhail Milekhin <mikhail.milekhin@gmail.com>
2019-02-17 12:32:34 +0300

    test lang en

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -206,3 +206,8 @@ latex_logo = '_static/img/logo.png'
 
 # Example configuration for intersphinx: refer to the Python standard library.
 # intersphinx_mapping = {'https://docs.python.org/': None
+
+
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False     # optional.
+gettext_uuid = True
