76c4d8baeeae664e80a5b779d199c6d929a63192
mtokarev <mtokarev@wavesplatform.com>
2019-07-11 19:22:54 +0300

    lang switcher fix

--- _static/docs-custom.js
+++ _static/docs-custom.js
@@ -7,4 +7,22 @@ function setDocLink() {
     menu.appendChild(div);
 }
 
-document.addEventListener("DOMContentLoaded", setDocLink);
+function setLangSwitcherLink() {
+    if (window.location.href.indexOf('/en/') === -1) {
+        document.getElementById('lang-ru')
+            .setAttribute(
+                "href",
+                window.location.origin + '/en' + window.location.pathname + window.location.hash
+            );
+    } else {
+        document.getElementById('lang-en')
+            .setAttribute("href", window.location.href.replace('/en', ''));
+    }
+}
+
+function main() {
+    setDocLink();
+    setLangSwitcherLink();
+}
+
+document.addEventListener("DOMContentLoaded", main);
