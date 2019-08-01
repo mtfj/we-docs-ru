3380e3159e182675c2676236d0f85f42de2f136e
Angela <aandronova@wavesplatform.com>
2019-08-01 15:50:12 +0300

    testing the versioning extention and adding info about max size of transactions

--- conf.py
+++ conf.py
@@ -45,6 +45,19 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     ]
 
+# Versioning settings
+author = 'exebeeche'
+project = 'Vostok'
+scv_greatest_tag = True
+scv_recent_tag = True
+scv_whitelist_branches = ('master', 'mayVostok')
+scv_banner_recent_tag = True
+scv_show_banner = True
+scv_banner_main_ref = 'feature_branch'
+scv_root_ref = 'feature_branch'
+
+
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
