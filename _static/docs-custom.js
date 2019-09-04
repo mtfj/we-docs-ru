c03e8a5e7dcb62522d50368417eb9d64de495c72
m-pashcin <9777953667@mail.ru>
2019-09-04 13:12:49 +0300

    mod JS-script

--- _static/docs-custom.js
+++ _static/docs-custom.js
@@ -1,14 +1,25 @@
+function delete_href()
+{
+let val=document.getElementsByTagName('html')[0].getAttribute('lang');
+ let val_lang='en';
+ 	if (val_lang==val) 
+	{ $('#rus_link').remove();}
+ }
+
 function setDocLink() {
     var div = document.createElement('div');
     div.className = 'doc-link';
     div.innerHTML =
-    '<div>' +
-        '<a class="reference internal" href="https://docs.wavesenterprise.com/vostokdocs.pdf">Документация</a>' +
+    '<div id="rus_link">' +
+        '<a  class="reference internal" href="https://docs.wavesenterprise.com/vostokdocs.pdf">Документация</a>' +
         '<span class="doc-link__label">.pdf</span>' +
-    '</div>' +
+    '</div>';
+	div.innerHTML=	 
     '<div class="version-text">Release 0.9.0</div>';
     var menu = document.querySelector('.wy-side-scroll');
     menu.appendChild(div);
+    delete_href();
+	
 }
 
 function setLangSwitcherLink() {
